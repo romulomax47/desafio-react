87d79ce6405ab25a1c42ca40ef02dba943894464
romulo max <maxromulo16@gmail.com>
2023-03-01 12:05:55 -0300

    refactor base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "desafio-react",
-  plugins: [react()],
+  base:"/home",
+  plugins: [react()]
 })
